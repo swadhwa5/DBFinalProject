a342aa979bba3ef4c454fa9b7d0bc6a77138fb33
swadhwa5 <swadhwa5@jh.edu>
2022-04-06 12:17:23 -0400

    added some files

---
+++ cleanup.sql
@@ -0,0 +1,9 @@
+DROP TABLE State
+DROP TABLE GrowsIn
+DROP TABLE Plant
+DROP TABLE ThreatenedPlant
+DROP TABLE FloweringPlant
+DROP TABLE Non-FloweringPlant
+DROP TABLE BotanicalGarden
+DROP TABLE FoodPlant
+DROP TABLE WaterBody
